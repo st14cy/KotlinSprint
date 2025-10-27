78b9b71ac5cd30b4ba89a9057bf1c6e1b99db9a3
st14cy <a2ium3d@gmail.ru>
2025-10-27 18:51:57 +0300

    KS-2-3 create task3

---
+++ src/main/kotlin/lesson_2/lesson2_task3.kt
@@ -0,0 +1,15 @@
+package org.example.lesson_2
+
+import kotlin.math.min
+
+fun main(){
+    val year: Int=1961
+    var hour: Short=9
+    var minute: Short=7
+
+    println("$year\n$hour\n$minute")
+
+    hour=10
+    minute=55
+    println("$hour:$minute")
+}
