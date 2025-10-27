7bcc77f92672371f89338c8d074cf4a91f45a672
st14cy <a2ium3d@gmail.ru>
2025-10-27 18:40:06 +0300

    Create 2-2 task

---
+++ src/main/kotlin/lesson_2/lesson2_task2.kt
@@ -0,0 +1,12 @@
+package org.example.lesson_2
+
+fun main(){
+    var orderCount: Int=31
+    val thankMessage: String="Спасибо за покупку!"
+    println("Кол-во заказов" + orderCount +" " + thankMessage )
+
+    var staffCount : Int =2000
+    //println(staffCount)
+    staffCount= 1999
+    println(staffCount)
+}
