83d4c85e76f8fd3ede77115950ffb685d8e4ff70
st14cy <a2ium3d@gmail.ru>
2025-10-28 18:39:33 +0300

    KS-2-5 Исправила автоформатирование

--- src/main/kotlin/lesson_2/lesson2_task5.kt
+++ src/main/kotlin/lesson_2/lesson2_task5.kt
@@ -1,10 +1,10 @@
 package org.example.lesson_2
 
 fun main() {
-    val second: Short = 6480
-    val totalHours = second / 60 / 60
-    val totalMinutes = second/ 60 % 60
-    val totalSecond=second % 60
+    val seconds: Short = 6480
+    val totalHours = seconds / 60 / 60
+    val totalMinutes = seconds / 60 % 60
+    val totalSecond = seconds % 60
 
     println(String.format("%02d:%02d:%02d", totalHours, totalMinutes, totalSecond))
 }
