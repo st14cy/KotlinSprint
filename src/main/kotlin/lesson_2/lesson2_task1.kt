b582db43964dd78c5b682f72c7fd3e628e4740f2
st14cy <a2ium3d@gmail.ru>
2025-10-28 18:27:40 +0300

    KS-2-1 удалила лишний файл и исправила автоформатирование

--- src/main/kotlin/lesson_2/lesson2_task1.kt
+++ src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -1,6 +1,6 @@
 package org.example.lesson_2
 
-fun main(){
-    var orderCount: Int=31
-    val thankMessage: String="Спасибо за покупку!"
+fun main() {
+    var orderCount: Int = 31
+    val thankMessage: String = "Спасибо за покупку!"
 }
