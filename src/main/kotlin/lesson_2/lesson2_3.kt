cb7939fc13616b70a725ba02ea82e1936a7f5373
st14cy <129183401+st14cy@users.noreply.github.com>
2025-10-29 13:44:46 +0500

    KS-2-3 рефакторинг

--- src/main/kotlin/lesson_2/lesson2_3.kt
+++ src/main/kotlin/lesson_2/lesson2_3.kt
@@ -4,12 +4,13 @@ fun main() {
     val hourDeparture = 9
     val minutesDeparture = 39
     val minutesOnTheWay = 457
-    val totalMinutesOnTheWay = hourDeparture * 60 + minutesDeparture + minutesOnTheWay
-    val totalHour = totalMinutesOnTheWay / 60
-    val totalMinutes = totalMinutesOnTheWay % 60
+    val oneHours = 60
+
+    val totalMinutesOnTheWay = hourDeparture * oneHours + minutesDeparture + minutesOnTheWay
+    val totalHour = totalMinutesOnTheWay / oneHours
+    val totalMinutes = totalMinutesOnTheWay % oneHours
+
     println("%02d:%02d".format(totalHour, totalMinutes))
 }
-
-
 
 
