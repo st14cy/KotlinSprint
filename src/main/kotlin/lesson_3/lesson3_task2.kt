eac97ab107fb6a2eec304bcb24002a63d5922f3e
st14cy <a2ium3d@gmail.ru>
2025-10-29 20:59:23 +0300

    KS-3-2 Выполнила задание

---
+++ src/main/kotlin/lesson_3/lesson3_task2.kt
@@ -0,0 +1,13 @@
+package org.example.lesson_3
+
+fun main() {
+    val name = "Татьяна"
+    val middleName = "Сергеевна"
+    var surname = "Андреева"
+    var year = 20
+    println("$surname $name $middleName, $year лет")
+
+    surname = "Сидорова"
+    year = 22
+    println("$surname $name $middleName, $year года")
+}
