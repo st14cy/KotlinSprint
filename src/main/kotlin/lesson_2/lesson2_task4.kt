2ca0959b0fe69e96f63c798d9d5a4028b0bf89d7
st14cy <129183401+st14cy@users.noreply.github.com>
2025-10-29 16:02:38 +0500

    KS-2-4 Выполнила задание

---
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -0,0 +1,15 @@
+package org.example.lesson_2
+
+fun main() {
+    val crystal = 7
+    val iron = 11
+    val buffProcent = 20
+
+    val crystalBuff = crystal * buffProcent / 100
+    val ironBuff = iron * buffProcent / 100
+
+    println("Кристаллическая руда: $crystalBuff")
+    println("Железная руда: $ironBuff")
+}
+
+
