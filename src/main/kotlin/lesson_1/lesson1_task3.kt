c34ff0834388e25e152ea9dc51ed82962b5b15cc
st14cy <a2ium3d@gmail.ru>
2025-10-29 20:19:17 +0300

    KS-1-3 Рефакторинг

---
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -0,0 +1,16 @@
+package org.example.lesson_1
+
+fun main() {
+    val year = 1961
+    var hour = 9
+    var minute = 7
+
+    println(year)
+    println(hour)
+    println(minute)
+
+    hour = 10
+    minute = 55
+
+    println("%02d:%02d".format(hour, minute))
+}
