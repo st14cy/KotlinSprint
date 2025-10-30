b77350d85603f56d9f2b7253310eb8f0efecb89f
st14cy <a2ium3d@gmail.ru>
2025-10-30 10:01:51 +0300

    KS-2-4 Рефакоторинг

--- src/main/kotlin/lesson_2/lesson2_task4.kt
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -1,15 +1,15 @@
 package org.example.lesson_2
 
+const val PROCENT = 100
+
 fun main() {
     val crystal = 7
     val iron = 11
     val buffProcent = 20
 
-    val crystalBuff = crystal * buffProcent / 100
-    val ironBuff = iron * buffProcent / 100
+    val crystalBuff = crystal * buffProcent / PROCENT
+    val ironBuff = iron * buffProcent / PROCENT
 
     println("Кристаллическая руда: $crystalBuff")
     println("Железная руда: $ironBuff")
 }
-
-
