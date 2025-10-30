c027403dcc530b40a7c165d0504c59650a085250
st14cy <a2ium3d@gmail.ru>
2025-10-30 09:58:33 +0300

    KS-2-5 Рефакоторинг

--- src/main/kotlin/lesson_2/lesson2_task5.kt
+++ src/main/kotlin/lesson_2/lesson2_task5.kt
@@ -1,16 +1,14 @@
 package org.example.lesson_2
 
+import kotlin.math.pow
+
+const val PROCENT = 100
+
 fun main() {
     var sum = 70_000.0
     val interestRate = 16.7
+    var year = 20
+    var finalValue = sum * (1 + interestRate / PROCENT).pow(year)
 
-    var i = 0
-    while (i < 20) {
-        sum += (sum * interestRate / 100)
-        i++;
-    }
-
-    println("%.3f".format(sum))
+    println("%.3f".format(finalValue))
 }
-
-
