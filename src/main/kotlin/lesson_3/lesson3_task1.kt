e8401de02999c0e32e664a56dce4288467921331
st14cy <a2ium3d@gmail.ru>
2025-10-30 10:07:37 +0300

    KS-3-1 Рефакоторинг

--- src/main/kotlin/lesson_3/lesson3_task1.kt
+++ src/main/kotlin/lesson_3/lesson3_task1.kt
@@ -4,7 +4,6 @@ fun main() {
     var name = "Илон Маск"
     val welcomeTextDay = "Добрый день"
     val welcomeTextEvening = "Добрый вечер"
-
     var welcomText = "$welcomeTextDay, $name!"
     println(welcomText)
 
