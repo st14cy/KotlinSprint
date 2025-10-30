09ee5e9b0738fbdf042ccd9d1bb8df3a78a05501
st14cy <a2ium3d@gmail.ru>
2025-10-30 13:37:53 +0300

    KS-2-5 Рефакторинг

--- src/main/kotlin/lesson_2/lesson2_task5.kt
+++ src/main/kotlin/lesson_2/lesson2_task5.kt
@@ -5,10 +5,10 @@ import kotlin.math.pow
 const val PROCENT = 100
 
 fun main() {
-    var sum = 70_000.0
+    val sum = 70_000.0
     val interestRate = 16.7
-    var year = 20
-    var finalValue = sum * (1 + interestRate / PROCENT).pow(year)
+    val year = 20
+    val finalValue = sum * (1 + interestRate / PROCENT).pow(year)
 
     println("%.3f".format(finalValue))
 }
