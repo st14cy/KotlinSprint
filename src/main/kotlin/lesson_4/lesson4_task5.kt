e3ce5150d1078be60a2806d882ac6467c54f080b
st14cy <129183401+st14cy@users.noreply.github.com>
2025-10-31 11:32:09 +0500

    KS-4-5 Исправила ошибку в переменной

--- src/main/kotlin/lesson_4/lesson4_task5.kt
+++ src/main/kotlin/lesson_4/lesson4_task5.kt
@@ -8,8 +8,8 @@ fun main() {
     println("Наличество ящиков с провизией на борту")
     var countProvisionBox = readln().toIntOrNull() ?: 0
     println("Благоприятность метеоусловий(true/false)")
-    var wether = readln().toBoolean()
+    var weather = readln().toBoolean()
 
-    println("${(!isDamage && countPerson in 55..70 && countProvisionBox > 50) || (isDamage && countPerson == 70 && wether && countProvisionBox >= 50)}")
+    println("${(!isDamage && countPerson in 55..70 && countProvisionBox > 50) || (isDamage && countPerson == 70 && weather && countProvisionBox >= 50)}")
 
 }
