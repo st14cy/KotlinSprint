15749cc5e5d9f94870fc69c61da9fe0628389f35
st14cy <129183401+st14cy@users.noreply.github.com>
2025-11-01 13:33:42 +0500

    KS-5-4 Выполнила задание

---
+++ src/main/kotlin/lesson_5/lesson5_task5.kt
@@ -0,0 +1,39 @@
+package org.example.lesson_5
+
+import kotlin.random.Random
+
+fun main() {
+    val lotteryNumbers = mutableListOf<Int>();
+    val personsNumbers = mutableListOf<Int>();
+    while (lotteryNumbers.size < 3) {
+        val num = Random.nextInt(0, 43)
+        if (num !in lotteryNumbers) {
+            lotteryNumbers.add(num)
+        }
+    }
+
+    println("${lotteryNumbers.get(0)},${lotteryNumbers.get(1)},${lotteryNumbers.get(2)},")
+
+    var countNum = 1
+    while (personsNumbers.size < 3) {
+        println("Введите ${countNum}e число: ");
+        var num = readln().trim().toInt()
+        if (num !in 0..42) {
+            println("Число должно быть в промежутке от 0 до 42");
+            return
+        }
+        countNum++
+        personsNumbers.add(num)
+    }
+
+    val matches = personsNumbers.intersect(lotteryNumbers)
+    val message = when (matches.size) {
+        1 -> "Вы угадали одно число и выиграли утешительный приз"
+        2 -> "Вы угадали два числа и выиграли крупный приз"
+        3 -> "Вы угадали все числа и выиграли джекпот"
+        else -> "Вы не угадали ни одного числа"
+    }
+    println(message)
+
+    println("Вы угадали: ${matches.joinToString(", ")}")
+}
